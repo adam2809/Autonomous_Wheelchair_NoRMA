8eb7df7a27b035f87e01c959c919587e54cbdbc5
Adam Kulesza <adam.kule@gmail.com>
2022-12-02 20:49:11 +0000

    Finishes joystick integration (google shit before u write it yourserlf moreal of the story)

--- src/joystick_driver/gazebo_driver.py
+++ src/joystick_driver/gazebo_driver.py
@@ -1,49 +1,60 @@
-from joystick_driver.base_driver import BaseJoystickDriver
+#!/usr/bin/env python
+
 import pygame
+import rospy
+from geometry_msgs.msg import Twist
+
 
 joy = []
+MAX_JOY_VAL = 1
+MIN_JOY_VAL = -1
+MAX_CMD_VEL_VAL = 0.5
+MIN_CMD_VEL_VAL = -0.5
 
-class GazeboJoystickDriver(BaseJoystickDriver):
+class GazeboJoystickDriver():
     def __init__(self):
+        rospy.init_node('joystick_control', anonymous=True)
+
         pygame.joystick.init()
         pygame.display.init()
 
         if not pygame.joystick.get_count():
-            print("Please connect a joystick and run again.")
+            rospy.loginfo("Please connect a joystick and run again.")
             quit()
 
-        print("%d joystick(s) detected." % pygame.joystick.get_count())
+        rospy.loginfo("%d joystick(s) detected." % pygame.joystick.get_count())
 
         for i in range(pygame.joystick.get_count()):
             myjoy = pygame.joystick.Joystick(i)
             myjoy.init()
             joy.append(myjoy)
-            print("Joystick %d: " % (i) + joy[i].get_name())
-            print("Depress trigger (button 0) to quit.")
+            rospy.loginfo("Joystick %d: " % (i) + joy[i].get_name())
+
+        self.twist = Twist()
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
         while True:
             e = pygame.event.wait()
-            handleJoyEvent(e)
+            self.handleJoyEvent(e)
 
 
-    def get_input(self):
-        #TODO should get this from keyboard input
-        pass
-
-
-    def set_output(self, fb, lr):
-        #TODO probably publish a topic to gazebo
-        pass
-
-
-    def handleJoyEvent(e):
+    def handleJoyEvent(self,e):
         axis_name_mapping = ['x','y','t']
         if e.type == pygame.JOYAXISMOTION:
             axis_i = e.dict['axis']
-            if (axis_i >= 0):
-                s = "%s | %f" % (axis_name_mapping[axis_i], e.dict['value'])
-                print(s)
-        if e.type == pygame.JOYBUTTONDOWN:
-            button_i = e.dict['button']
-            if(button_i >= 0):
-                print(f'b | {button_i}')
+            cmd_vel_val = self.scale_joy_val_to_cmd_vel(e.dict['value'])
+            if axis_i == 0:
+                self.twist.angular.z = cmd_vel_val
+            if axis_i == 1:
+                self.twist.linear.x = cmd_vel_val
+            rospy.loginfo("%s | %f" % (axis_name_mapping[axis_i], e.dict['value']))
+            self.cmd_vel_pub.publish(self.twist)
+
+    def scale_joy_val_to_cmd_vel(self,joy_val):
+        return -(((joy_val - MIN_JOY_VAL) / (MAX_JOY_VAL - MIN_JOY_VAL) ) * (MAX_CMD_VEL_VAL - MIN_CMD_VEL_VAL) + MIN_CMD_VEL_VAL)
+
+
+
+if __name__ == '__main__':
+   GazeboJoystickDriver()
+
